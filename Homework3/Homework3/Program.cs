41b118684f099941d5afbf06d204b5c878fd7d15
Jovanche16 <jovance.sarafinoski@gmail.com>
2025-03-16 16:43:34 +0100

    Uploading the 3rd homework

---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,38 @@
+﻿
+static DateTime GetValidDate()
+{
+    DateTime birthDate;
+    while (true)
+    {
+        Console.Write("Enter your birthdate: ");
+        string input = Console.ReadLine();
+
+        if (DateTime.TryParse(input, out birthDate))
+        {
+            return birthDate;
+        }
+        else
+        {
+            Console.WriteLine("Invalid date format. Please enter a valid date!");
+        }
+    }
+}
+static int AgeCalculator(DateTime birthDate)
+{
+    DateTime today = DateTime.Today;
+    int age = today.Year - birthDate.Year;
+
+    if (birthDate.Date > today.AddYears(-age))
+    {
+        age--;
+    }
+
+    return age;
+}
+
+DateTime birthDate = GetValidDate();
+int age = AgeCalculator(birthDate);
+Console.WriteLine($"You are {age} years old.");
+
+Console.WriteLine();
+
